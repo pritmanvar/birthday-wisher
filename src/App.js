986d9030a294c1656aa44fb5cc6d46208de64c9e
pritmanvar <pritmanvar1109@gmail.com>
2022-06-04 10:11:27 +0530

    Changed name of heading and added some comments.

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ const BirthdayAudio = new Audio(BirthdaySong);
 BirthdayAudio.loop = true;
 
 function App() {
-  const [handleOpacity,setHandleOpacity] = useState({opacity: '1'});
-  const [handleZIndex,setHandleZIndex] = useState({zIndex: '1'});
-  const [candlesBlowen,setCandlesBlowen] = useState(false);
-  const [candlesBlowenSignal,setCandlesBlowenSignal] = useState(false);
+  const [handleOpacity,setHandleOpacity] = useState({opacity: '1'}); // to handle opacity of dark screen
+  const [handleZIndex,setHandleZIndex] = useState({zIndex: '1'}); // handle z-index of dark screen. if i don't change it then minion will not be clickable
+  const [candlesBlowen,setCandlesBlowen] = useState(false); // candles are blown or not. if it is blown then i have to display wish.
+  const [candlesBlowenSignal,setCandlesBlowenSignal] = useState(false); // signal to blow candle. when it is true i have to blow the candles.
 
-  const handleLights = ()=>{
-    if(handleOpacity.opacity === '1'){
+  const handleLights = ()=>{ // function to turn on and turn off lights.
+    if(handleOpacity.opacity === '1'){ // turn on lights
       setHandleOpacity({opacity: '0'});
       if(document.getElementById('birthdayHeading') !== null)
       document.getElementById('birthdayHeading').classList.add('wishAppear');
       document.getElementById('balloon1').classList.add('animate1');
       document.getElementById('balloon2').classList.add('animate2');
-    }else{
+    }else{ // turn off lights -> show coverElement and hide main elements
       setHandleOpacity({opacity: '1'});
       setHandleZIndex({zIndex: '1'});
     }
   }
   
-  useEffect(()=>{
+  useEffect(()=>{ // run this and re-render app component whenever handleOpacity changes
     setTimeout(() => {
       if(handleOpacity.opacity === '0'){
         setHandleZIndex({zIndex: '-1'});    
@@ -38,7 +38,7 @@ function App() {
       }
     }, 1000);
   }, [handleOpacity]);
-  useEffect(()=>{
+  useEffect(()=>{ // run this and re-render app component whenever candlesBlowenSignal changes
     if(candlesBlowenSignal === true){
       setHandleOpacity({opacity: '1'})
       setHandleZIndex({zIndex: '1'});
--- src/components/BirthdayHeading.js
+++ src/components/BirthdayHeading.js
@@ -4,7 +4,7 @@ import '../style/birthdayHeading.css'
 function BirthdayHeading() {
   return (
     <>
-      <p className="birthdayHeading" id="birthdayHeading">Happy Birthday<br/>Suketu</p>
+      <p className="birthdayHeading" id="birthdayHeading">Happy Birthday<br/>Drishty</p>
     </>
   )
 }
