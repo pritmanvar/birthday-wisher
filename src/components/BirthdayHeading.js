d060e5ae6fef72f82aa361f7fe036e95ad5ae583
pritmanvar <pritmanvar1109@gmail.com>
2022-07-20 18:41:16 +0530

    minor change

--- src/components/BirthdayHeading.js
+++ src/components/BirthdayHeading.js
@@ -4,7 +4,7 @@ import '../style/birthdayHeading.css'
 function BirthdayHeading() {
   return (
     <>
-      <p className="birthdayHeading" id="birthdayHeading">Happy Birthday<br/>Drishty</p>
+      <p className="birthdayHeading" id="birthdayHeading">Happy Birthday<br/>Name</p>
     </>
   )
 }
